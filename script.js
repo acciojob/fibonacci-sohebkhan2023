01e3b82f697a9d8145a80e8b76eeb6348ed8cc79
Sohebkhan <102939844+sohebkhan2023@users.noreply.github.com>
2023-07-08 13:23:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,10 @@
 function fibonacci(num) 
 {
 // your code here
-	let f1=0;
-	let f2=1;
-	if(num=1)
-		return 0;
-	else if(num=2)
-		return 1;
-	else 
-		for(let i=2;i<=num;i++)
-		{
-			let f3=f1+f2;
-			f1=f2;
-			f2=f3;
-			return f3;
-			
-		}
+	if(n === 1)return 0;
+	if(n === 2)return 1;
+
+	return fibonacci(num-1)+fibonacci(num-2);
 	
 
 
