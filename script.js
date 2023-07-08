cf7ceac948212a57e1074e485fef701caebcab0c
Sohebkhan <102939844+sohebkhan2023@users.noreply.github.com>
2023-07-08 13:16:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-function fibonacci(num) {
+function fibonacci(num) 
+{
 // your code here
+	let f1=0;
+	let f2=1;
+	if(num=1)
+		return 0;
+	if(num=2)
+		return 1;
+	for(let i=2;i<=num;i++)
+		{
+			let f3=f1+f2;
+			f1=f2;
+			f2=f3;
+			return f3;
+			
+		}
+	
+
+
 }
 
-module.exports = fibonacci;
+
